233c880982037447bb0e144fa76cad5385f2f61b
Malouzi <marie.zirzlaff@dci-student.org>
2025-02-17 12:58:30 +0100

    Ordner

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+export function Navbar() {
+    return (
+        <>
+          <nav className='navbar'>
+            <div className='nav-part nav-left-side'>
+              <li>Logo</li>
+              <li>Produkte</li>
+              <li>Generator</li>
+            </div>
+            <div className='nav-part nav-right-side'>
+              <li>Suche</li>
+              <li>Kontakt</li>
+            </div>
+          </nav>
+        </>
+      )
+}
