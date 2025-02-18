6038268cd9cd4594707582211e4f0ce23227435f
Malouzi <marie.zirzlaff@dci-student.org>
2025-02-18 13:27:40 +0100

    Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+   <>
+   <footer className="footer">
+    <div className="logo-container">
+        <h3>Schatzbox</h3>
+    </div>
+    <div className="footer-links-container">
+            <li>Impressum</li>
+            <li>Datenschutz</li>
+            <li>Kontakt</li>
+    </div>
+   </footer>
+   </>
+  );
+}
