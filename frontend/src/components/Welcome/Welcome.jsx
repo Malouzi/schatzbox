54c1cdaeb51cc1f1b9205bd50cb8aa4fbf6c6bbb
simisunday <simia_karegar@live.de>
2025-02-18 12:58:54 +0100

    welcome text

---
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -0,0 +1,23 @@
+import "./Welcome.css";
+
+export function Welcome() {
+  return (
+    <div>
+      <h1>Willkommen bei Schatzbox</h1>
+      <p>
+        Plane den perfekten Kindergeburtstag mit unseren spannenden
+        Schatzsuch-Vorlagen! Bei uns findest du liebevoll gestaltete Abenteuer,
+        die Du mit einem Mausklick kaufen, downloaden und ausdrucken kannst. Ob
+        Piraten, Detektive oder magische Welten – wir haben die passende
+        Schatzsuche für jedes Kind! Unsere Schatzboxen sind entweder nach Thema
+        wählbar oder wir stellen individuell für Euch eine Schatzsuche nach ganz
+        ersönlichen Wünschen. Nutze dafür unsere individuelle Schatzsuche.
+        Sofort-Download – Direkt nach dem Kauf verfügbar Einfache Anleitung –
+        Schnell vorbereitet & los geht’s Perfekt für drinnen & draußen –
+        Flexible Nutzung Entdecke jetzt die passende Schatzsuche für Deine
+        Liebsten und machen nächsten Kindergeburtstag zu einem unvergesslichen
+        Erlebnis!
+      </p>
+    </div>
+  );
+}
