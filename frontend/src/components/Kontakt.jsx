72fcb5bc173cafab0840e5d97db6d3be17598296
Malouzi <marie.zirzlaff@dci-student.org>
2025-02-19 15:19:57 +0100

    Logo neu, Router Links Navbar

---
+++ frontend/src/components/Kontakt.jsx
@@ -0,0 +1,13 @@
+export default function Kontakt() {
+  return (
+    <div className="kontakt-page">
+      <h1>Kontakt</h1>
+      <p>So können Sie uns erreichen:</p>
+      <ul>
+        <li>Telefon: 0123 456789</li>
+        <li>Email: info@beispiel.de</li>
+        <li>Adresse: Musterstraße 1, 12345 Musterstadt</li>
+      </ul>
+    </div>
+  );
+}
