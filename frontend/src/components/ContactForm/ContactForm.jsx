6fd70461f8453b014067ec2122a1314182a00a3f
simisunday <simia_karegar@live.de>
2025-02-24 11:09:37 +0100

    Kontaktformular läuft

--- frontend/src/components/ContactForm/ContactForm.jsx
+++ frontend/src/components/ContactForm/ContactForm.jsx
@@ -2,40 +2,40 @@ import React from "react";
 import "../ContactForm/ContactForm.css";
 
 export default function ContactForm() {
-        const [result, setResult] = React.useState("");
-      
-        const onSubmit = async (event) => {
-          event.preventDefault();
-          setResult("Sending....");
-          const formData = new FormData(event.target);
-      
-          formData.append("access_key", "bc8821f0-66f6-4b7f-875e-5c74ed828950");
-      
-          const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            body: formData
-          });
-      
-          const data = await response.json();
-      
-          if (data.success) {
-            setResult("Form Submitted Successfully");
-            event.target.reset();
-          } else {
-            console.log("Error", data);
-            setResult(data.message);
-          }
-        };
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bc8821f0-66f6-4b7f-875e-5c74ed828950");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <section className="contact">
-      <form>
+      <form onSubmit={onSubmit}>
         <h2>Kontaktformular</h2>
         <div className="input-box">
           <label></label>
           <input
             type="text"
             className="field"
-            placeholder="Vorname*" 
+            placeholder="Vorname*"
             name="vorname"
             required
           />
@@ -54,18 +54,14 @@ export default function ContactForm() {
           <input
             type="text"
             className="field"
-            placeholder="Deine E-Mail Adresse*" 
-            name="email" 
+            placeholder="Deine E-Mail Adresse*"
+            name="email"
             required
           />
         </div>
         <div className="input-box">
           <label></label>
-          <input
-            type="text"
-            className="field"
-            placeholder="telefon"
-          />
+          <input type="text" className="field" placeholder="telefon" />
         </div>
         <div className="input-box">
           <label></label>
@@ -73,13 +69,20 @@ export default function ContactForm() {
             type="text"
             className="field"
             placeholder="Betreff*"
-            name="betreff" 
+            name="betreff"
             required
           />
         </div>
         <div className="input-box">
           <label></label>
-          <textarea name="nachricht" id="" placeholder="Nachricht*" cols="30" rows="10" required></textarea>
+          <textarea
+            name="nachricht"
+            id=""
+            placeholder="Nachricht*"
+            cols="30"
+            rows="10"
+            required
+          ></textarea>
         </div>
         <button type="submit">absenden</button>
       </form>
