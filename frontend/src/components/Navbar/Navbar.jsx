6a23f0731a8125e4c37ebd10cb34e663b23fb4f1
simisunday <simia_karegar@live.de>
2025-02-26 09:25:20 +0100

    navbar angepasst

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo-transparent.png";
-import Cart from '../Cart/Cart';
+import Cart from "../Cart/Cart";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,26 +19,32 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className={`navbar-right ${isOpen ? 'open' : ''}`}>
+      <div className={`navbar-left ${isOpen ? "open" : ""}`}>
         <div className="hamburger" onClick={toggleMenu}>
           <span></span>
           <span></span>
           <span></span>
         </div>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/angebote">Angebote</Link></li>
-          <li><Link to="/kontakt">Kontakt</Link></li>
           <li>
-            <button className="cart-icon" onClick={toggleCart}>
-              🛒
-            </button>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/angebote">Angebote</Link>
+          </li>
+          <li>
+            <Link to="/kontakt">Kontakt</Link>
           </li>
         </ul>
-        {showCart && <Cart />}
+        <div className="navbar-middle">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="navbar-right">
+          <button className="cart-icon" onClick={toggleCart}>
+            🛒
+          </button>
+          {showCart && <Cart />}
+        </div>
       </div>
     </nav>
   );
