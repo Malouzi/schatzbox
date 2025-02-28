5ec7b25461a4d3adb17db985c6b724ad69d36d90
Malouzi <marie.zirzlaff@dci-student.org>
2025-02-28 09:42:45 +0100

    Setup backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+config();
+
+const app = express();
+app.use(cors());
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+
+mongoose
+.connect(process.env.mongoDB)
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Error connecting to MongoDB:', error));
