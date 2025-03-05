bde6286e3d076ca7db73468a8a94c068a1093cdb
Malouzi <marie.zirzlaff@dci-student.org>
2025-03-05 10:19:20 +0100

    Compass verbunden

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import { config } from 'dotenv';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import { v2 as cloudinary } from 'cloudinary';
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import productRoute from './routes/productRoute.js';
+import { connect } from './utils/db.js';
 
 config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
-
-mongoose
-    .connect(process.env.mongoDB)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.log('Error connecting to MongoDB:', error));
-
-app.use(express.json());
-
 app.use('/product', productRoute);
+
+
+//Error Middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    return res.sendStatus(500);
+  });
+  
+  
+  // Zuvor müssen alle Verbindungen stehen, bevor der Server offengelegt
+  connect().then(() => {
+    app.listen(PORT, () => console.log(`Listen at http://localhost:${PORT}`));
+  });
