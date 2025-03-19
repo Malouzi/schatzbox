5e92be8777a15383abe4ecdc24f5046dc6c67eb2
simisunday <simia_karegar@live.de>
2025-03-19 10:13:13 +0100

    neue ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,40 +1,46 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
-import styles from './ProductDetails.module.css';
-
+import styles from './productDetails.module.css';
+import productsData from '../../public/books.json';
 const ProductDetails = () => {
   const { id } = useParams();
   const { addToCart } = useContext(CartContext);
-  
-  // Hier sollten die Produktdaten abgerufen werden, z.B. von einer API oder aus einem lokalen Array
-  const product = {
-    id: id,
-    title: 'Beispiel Produkt',
-    description: 'Dies ist eine detaillierte Beschreibung des Produkts.',
-    price: 29.99,
-    coverImage: 'path/to/image.jpg',
-    rating: 4.5,
-    availability: true,
-  }; 
-
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchProduct = () => {
+      const foundProduct = productsData.find((item) => item._id === parseInt(id));
+      if (foundProduct) {
+        setProduct(foundProduct);
+      } else {
+        setError("Produkt nicht gefunden");
+      }
+      setLoading(false);
+    };
+    fetchProduct();
+  }, [id]);
+  if (loading) {
+    return <p>Lädt...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   const handleAddToCart = () => {
     addToCart(product);
     alert(`${product.title} wurde zum Warenkorb hinzugefügt!`);
   };
-
-  return ( 
-
+  return (
     <div className={styles.productDetailsContainer}>
-      <h2>{product.title}</h2>
+      <h2>{product.name}</h2>
       <img src={`/${product.coverImage}`} alt={product.title} />
       <p>{product.description}</p>
       <p>Preis: {product.price} €</p>
       <p>Bewertung: {product.rating} ★</p>
       <p>Verfügbarkeit: {product.availability ? 'Auf Lager' : 'Nicht verfügbar'}</p>
-      <button onClick={handleAddToCart}>In den Warenkorb</button>
+      <button onClick={handleAddToCart} disabled={!product.availability}>In den Warenkorb</button>
     </div>
   );
 };
-
 export default ProductDetails;
