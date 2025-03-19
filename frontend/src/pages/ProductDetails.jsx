4d2329c3e3d80d2d364f0ff9a05d809c24b5f256
Malouzi <marie.zirzlaff@dci-student.org>
2025-03-19 10:40:16 +0100

    ProduktDetails geändert

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -3,29 +3,35 @@ import { useParams } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
 import styles from './productDetails.module.css';
 import productsData from '../../public/books.json';
-
 const ProductDetails = () => {
   const { id } = useParams();
   const { addToCart } = useContext(CartContext);
-  
-  // Hier sollten die Produktdaten abgerufen werden, z.B. von einer API oder aus einem lokalen Array
-  const product = {
-    id: id,
-    title: 'Beispiel Produkt',
-    description: 'Dies ist eine detaillierte Beschreibung des Produkts.',
-    price: 29.99,
-    coverImage: 'path/to/image.jpg',
-    rating: 4.5,
-    availability: true,
-  }; 
-
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchProduct = () => {
+      const foundProduct = productsData.find((item) => item._id === parseInt(id));
+      if (foundProduct) {
+        setProduct(foundProduct);
+      } else {
+        setError("Produkt nicht gefunden");
+      }
+      setLoading(false);
+    };
+    fetchProduct();
+  }, [id]);
+  if (loading) {
+    return <p>Lädt...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   const handleAddToCart = () => {
     addToCart(product);
     alert(`${product.title} wurde zum Warenkorb hinzugefügt!`);
   };
-
-  return ( 
-
+  return (
     <div className={styles.productDetailsContainer}>
       <h2>{product.name}</h2>
       <img src={`/${product.coverImage}`} alt={product.title} />
